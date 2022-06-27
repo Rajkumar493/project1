f09e304d783f52a07e1985bd582d4af34029dcea
Rajkumar493 <107843522+Rajkumar493@users.noreply.github.com>
2022-06-27 15:07:56 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld {
   public static void main(String[] args) {
     System.out.println("Hello World");
-    System.out.println("its my new project ");
+    System.out.println("......its my new project...... ");
   }
 }
 
